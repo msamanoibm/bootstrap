60062a562dba726e87f372c65d38fe1b546cb0e9
Mike Samano <michael.samano@hcl.com>
2020-03-02 11:32:20 -0800

    Add user-data script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-cp -a ubuntu/.bashrc ~/
-sudo cp -a bash/.bashrc /root/
+sudo -u ubuntu cp -v -a ubuntu/.bashrc /home/ubuntu/
+cp -v -a root/.bashrc /root/
 
-cp -a bash/.inputrc ~/
-sudo cp -a bash/.inputrc /root/
+sudo -u ubuntu cp -v -a bash/.inputrc /home/ubuntu/
+cp -v -a bash/.inputrc /root/
 
-cp -a bash/.bash/ ~
-sudo cp -a bash/.bash/ /root/
+sudo -u ubuntu cp -v -a bash/.bash/ /home/ubuntu/
+cp -v -a bash/.bash/ /root/
 
-sudo apt -yqq update
-sudo apt -yqq upgrade
-sudo apt -yqq install docker.io vim groovy openjdk-8-jdk curl unzip ping telnet
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-curl -O http://public.dhe.ibm.com/software/products/UrbanCode/Velocity/latest/velocity-se-ibm-install-latest-linux
+apt -yqq update
+apt -yqq upgrade
+apt -yqq install docker.io vim groovy openjdk-8-jdk curl unzip telnet
+cd /home/ubuntu
+sudo -u ubuntu curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo -u ubuntu curl -O http://public.dhe.ibm.com/software/products/UrbanCode/Velocity/latest/velocity-se-ibm-install-latest-linux
 chmod 755 velocity-se-ibm-install-latest-linux
+sudo -u ubuntu mkdir velocity
+ufw deny 443
